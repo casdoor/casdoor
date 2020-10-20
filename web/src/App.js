bb06e55d8dccb4a5a03abf6689c5fa08eea9263d
Yang Luo <hsluoyz@qq.com>
2020-10-20 22:06:49 +0800

    Add antd.

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button} from "antd";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
