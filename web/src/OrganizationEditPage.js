acaee2e8922af49d1459c7e8e26035f608986432
Gucheng Wang <nomeguy@qq.com>
2021-11-06 15:01:51 +0800

    Fix "Phone prefix" text typo.

--- web/src/OrganizationEditPage.js
+++ web/src/OrganizationEditPage.js
@@ -170,7 +170,7 @@ class OrganizationEditPage extends React.Component {
         </Row>
         <Row style={{marginTop: '20px'}} >
           <Col style={{marginTop: '5px'}} span={(Setting.isMobile()) ? 22 : 2}>
-            {Setting.getLabel(i18next.t("general:Phone Prefix"), i18next.t("general:Phone Prefix - Tooltip"))} :
+            {Setting.getLabel(i18next.t("general:Phone prefix"), i18next.t("general:Phone prefix - Tooltip"))} :
           </Col>
           <Col span={22} >
             <Input addonBefore={"+"} value={this.state.organization.phonePrefix} onChange={e => {
