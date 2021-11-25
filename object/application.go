7f39aee9c49111712839d19f476c4c60cfb58bff
Gucheng Wang <nomeguy@qq.com>
2021-11-25 16:44:49 +0800

    Make application's fields longer.

--- object/application.go
+++ object/application.go
@@ -39,10 +39,10 @@ type Application struct {
 	ClientSecret   string   `xorm:"varchar(100)" json:"clientSecret"`
 	RedirectUris   []string `xorm:"varchar(1000)" json:"redirectUris"`
 	ExpireInHours  int      `json:"expireInHours"`
-	SignupUrl      string   `xorm:"varchar(100)" json:"signupUrl"`
-	SigninUrl      string   `xorm:"varchar(100)" json:"signinUrl"`
-	ForgetUrl      string   `xorm:"varchar(100)" json:"forgetUrl"`
-	AffiliationUrl string   `xorm:"varchar(100)" json:"affiliationUrl"`
+	SignupUrl      string   `xorm:"varchar(1000)" json:"signupUrl"`
+	SigninUrl      string   `xorm:"varchar(1000)" json:"signinUrl"`
+	ForgetUrl      string   `xorm:"varchar(1000)" json:"forgetUrl"`
+	AffiliationUrl string   `xorm:"varchar(1000)" json:"affiliationUrl"`
 	TermsOfUse     string   `xorm:"varchar(1000)" json:"termsOfUse"`
 	SignupHtml     string   `xorm:"mediumtext" json:"signupHtml"`
 	SigninHtml     string   `xorm:"mediumtext" json:"signinHtml"`
