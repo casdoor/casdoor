80bb7c30611eaaaea180e9d02bbe5b16aef02059
Rafael Firmino <rafaelgfirmino@gmail.com>
2022-02-09 22:57:53 -0300

    Fix Copyright position

--- object/syncer_user_test.go
+++ object/syncer_user_test.go
@@ -1,5 +1,4 @@
-// +build !skipCi
-//
+// Copyright 2021 The casbin Authors. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,6 +10,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+// +build !skipCi
 
 package object
 
