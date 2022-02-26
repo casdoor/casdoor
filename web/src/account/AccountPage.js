2118306655524f64a1d442ff2a07f5b4f8786511
Yixiang Zhao <seriouszyx@foxmail.com>
2022-02-26 11:04:46 +0800

    fix: this.props.location undefined
    Signed-off-by: Yixiang Zhao <seriouszyx@foxmail.com>

--- web/src/account/AccountPage.js
+++ web/src/account/AccountPage.js
@@ -18,7 +18,7 @@ import UserEditPage from "../UserEditPage";
 class AccountPage extends React.Component {
   render() {
     return (
-      <UserEditPage organizationName={this.props.account.owner} userName={this.props.account.name} account={this.props.account} />
+      <UserEditPage organizationName={this.props.account.owner} userName={this.props.account.name} account={this.props.account} location={this.props.location} />
     )
   }
 }
