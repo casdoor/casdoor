0e0cc744ee29d444da63349938f9496c9e109cf4
Nekotoxin <nekotoxin@outlook.com>
2022-03-04 09:21:10 +0800

    fix: fix the No.0 bug(for all sign up methods)

--- controllers/account.go
+++ controllers/account.go
@@ -149,8 +149,6 @@ func (c *ApiController) Signup() {
 		username = id
 	}
 
-	userCount := object.GetUserCount(form.Organization, "", "") + 1
-
 	user := &object.User{
 		Owner:             form.Organization,
 		Name:              username,
@@ -173,7 +171,6 @@ func (c *ApiController) Signup() {
 		IsDeleted:         false,
 		SignupApplication: application.Name,
 		Properties:        map[string]string{},
-		Ranking:           userCount + 1,
 		Karma:             0,
 	}
 
