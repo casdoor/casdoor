743dcc972509ebcf7bb9640d046eab539c51c105
Yang Luo <hsluoyz@qq.com>
2022-03-12 23:37:50 +0800

    Fix translation.

--- web/src/OrganizationEditPage.js
+++ web/src/OrganizationEditPage.js
@@ -113,7 +113,7 @@ class OrganizationEditPage extends React.Component {
         </Row>
         <Row style={{marginTop: '20px'}} >
           <Col style={{marginTop: '5px'}} span={(Setting.isMobile()) ? 22 : 2}>
-            {Setting.getLabel("general:Favicon", i18next.t("general:Favicon - Tooltip"))} :
+            {Setting.getLabel( i18next.t("general:Favicon"), i18next.t("general:Favicon - Tooltip"))} :
           </Col>
           <Col span={22} >
             <Row style={{marginTop: '20px'}} >
