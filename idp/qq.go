c37a5199c04422c3b0086edb306bb095a5249123
Steve0x2a <stevesough@gmail.com>
2022-04-01 03:30:45 +0000

    fix: qq idp missing username
    Signed-off-by: Steve0x2a <stevesough@gmail.com>

--- idp/qq.go
+++ idp/qq.go
@@ -76,6 +76,9 @@ func (idp *QqIdProvider) GetToken(code string) (*oauth2.Token, error) {
 
 	defer resp.Body.Close()
 	tokenContent, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
 	re := regexp.MustCompile("token=(.*?)&")
 	matched := re.FindAllStringSubmatch(string(tokenContent), -1)
@@ -87,54 +90,30 @@ func (idp *QqIdProvider) GetToken(code string) (*oauth2.Token, error) {
 	return token, nil
 }
 
-//{
-//	"ret": 0,
-//	"msg": "",
-//	"is_lost": 0,
-//	"nickname": "飞翔的企鹅",
-//	"gender": "男",
-//	"gender_type": 1,
-//	"province": "",
-//	"city": "安道尔城",
-//	"year": "1968",
-//	"constellation": "",
-//	"figureurl": "http:\/\/qzapp.qlogo.cn\/qzapp\/101896710\/C0D022F92B604AA4B1CDF92CC79463A4\/30",
-//	"figureurl_1": "http:\/\/qzapp.qlogo.cn\/qzapp\/101896710\/C0D022F92B604AA4B1CDF92CC79463A4\/50",
-//	"figureurl_2": "http:\/\/qzapp.qlogo.cn\/qzapp\/101896710\/C0D022F92B604AA4B1CDF92CC79463A4\/100",
-//	"figureurl_qq_1": "http://thirdqq.qlogo.cn/g?b=oidb&k=QtAu5OiaSfqGD0kfclwvxJA&s=40&t=1557635654",
-//	"figureurl_qq_2": "http://thirdqq.qlogo.cn/g?b=oidb&k=QtAu5OiaSfqGD0kfclwvxJA&s=100&t=1557635654",
-//	"figureurl_qq": "http://thirdqq.qlogo.cn/g?b=oidb&k=QtAu5OiaSfqGD0kfclwvxJA&s=640&t=1557635654",
-//	"figureurl_type": "1",
-//	"is_yellow_vip": "0",
-//	"vip": "0",
-//	"yellow_vip_level": "0",
-//	"level": "0",
-//	"is_yellow_year_vip": "0"
-//}
+/*
+{
+	"ret":0,
+	"msg":"",
+	"nickname":"Peter",
+	"figureurl":"http://qzapp.qlogo.cn/qzapp/111111/942FEA70050EEAFBD4DCE2C1FC775E56/30",
+	"figureurl_1":"http://qzapp.qlogo.cn/qzapp/111111/942FEA70050EEAFBD4DCE2C1FC775E56/50",
+	"figureurl_2":"http://qzapp.qlogo.cn/qzapp/111111/942FEA70050EEAFBD4DCE2C1FC775E56/100",
+	"figureurl_qq_1":"http://q.qlogo.cn/qqapp/100312990/DE1931D5330620DBD07FB4A5422917B6/40",
+	"figureurl_qq_2":"http://q.qlogo.cn/qqapp/100312990/DE1931D5330620DBD07FB4A5422917B6/100",
+	"gender":"男"
+}
+*/
 
 type QqUserInfo struct {
-	Ret             int    `json:"ret"`
-	Msg             string `json:"msg"`
-	IsLost          int    `json:"is_lost"`
-	Nickname        string `json:"nickname"`
-	Gender          string `json:"gender"`
-	GenderType      int    `json:"gender_type"`
-	Province        string `json:"province"`
-	City            string `json:"city"`
-	Year            string `json:"year"`
-	Constellation   string `json:"constellation"`
-	Figureurl       string `json:"figureurl"`
-	Figureurl1      string `json:"figureurl_1"`
-	Figureurl2      string `json:"figureurl_2"`
-	FigureurlQq1    string `json:"figureurl_qq_1"`
-	FigureurlQq2    string `json:"figureurl_qq_2"`
-	FigureurlQq     string `json:"figureurl_qq"`
-	FigureurlType   string `json:"figureurl_type"`
-	IsYellowVip     string `json:"is_yellow_vip"`
-	Vip             string `json:"vip"`
-	YellowVipLevel  string `json:"yellow_vip_level"`
-	Level           string `json:"level"`
-	IsYellowYearVip string `json:"is_yellow_year_vip"`
+	Ret          int    `json:"ret"`
+	Msg          string `json:"msg"`
+	Nickname     string `json:"nickname"`
+	Gender       string `json:"gender"`
+	Figureurl    string `json:"figureurl"`
+	Figureurl1   string `json:"figureurl_1"`
+	Figureurl2   string `json:"figureurl_2"`
+	FigureurlQq1 string `json:"figureurl_qq_1"`
+	FigureurlQq2 string `json:"figureurl_qq_2"`
 }
 
 func (idp *QqIdProvider) GetUserInfo(token *oauth2.Token) (*UserInfo, error) {
@@ -146,7 +125,10 @@ func (idp *QqIdProvider) GetUserInfo(token *oauth2.Token) (*UserInfo, error) {
 
 	defer resp.Body.Close()
 	openIdBody, err := ioutil.ReadAll(resp.Body)
-
+	if err != nil {
+		return nil, err
+	}
+	
 	re := regexp.MustCompile("\"openid\":\"(.*?)\"}")
 	matched := re.FindAllStringSubmatch(string(openIdBody), -1)
 	openId := matched[0][1]
@@ -178,6 +160,7 @@ func (idp *QqIdProvider) GetUserInfo(token *oauth2.Token) (*UserInfo, error) {
 
 	userInfo := UserInfo{
 		Id:          openId,
+		Username:    qqUserInfo.Nickname,
 		DisplayName: qqUserInfo.Nickname,
 		AvatarUrl:   qqUserInfo.FigureurlQq1,
 	}
