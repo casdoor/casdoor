5a5658145b3329eab6574cf041a2d5321bd6a84e
Yang Luo <hsluoyz@qq.com>
2022-05-02 17:18:16 +0800

    Update SignupTable.js

--- web/src/SignupTable.js
+++ web/src/SignupTable.js
@@ -176,7 +176,7 @@ class SignupTable extends React.Component {
           } else if (record.name === "Email") {
             options = [
               {id: 'Normal', name: 'Normal'},
-              {id: 'No Verification', name: 'No Verification'},
+              {id: 'No Verification', name: 'No verification'},
             ];
           }
 
