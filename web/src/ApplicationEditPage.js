218d74784a486a867cbdeefbb39505972bec6bdc
Yang Luo <hsluoyz@qq.com>
2022-05-04 10:02:21 +0800

    Update ApplicationEditPage.js

--- web/src/ApplicationEditPage.js
+++ web/src/ApplicationEditPage.js
@@ -454,9 +454,9 @@ class ApplicationEditPage extends React.Component {
                           {id: "password", name: "Password"},
                           {id: "client_credentials", name: "Client Credentials"},
                           {id: "token", name: "Token"},
-                          {id: "id_token", name:"ID Token"},
-                          {id: "refresh_token", name:"Refresh Token"},
-                        ].map((item, index)=><Option key={index} value={item.id}>{item.name}</Option>)
+                          {id: "id_token", name: "ID Token"},
+                          {id: "refresh_token", name: "Refresh Token"},
+                        ].map((item, index) => <Option key={index} value={item.id}>{item.name}</Option>)
                       }
             </Select>
           </Col>
