5dac87a4c3000780ae21368886f97952531b6c8b
Товарищ программист <46831212+ComradeProgrammer@users.noreply.github.com>
2022-05-07 20:56:12 +0800

    feat: hide proxy-test output (#746)
    * feat: hide proxy-test output
    
    * Update build.sh
    
    Co-authored-by: Yang Luo <hsluoyz@qq.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #try to connect to google to determine whether user need to use proxy
-curl www.google.com -o /dev/null --connect-timeout 5
+curl www.google.com -o /dev/null --connect-timeout 5 2 > /dev/null
 if [ $? == 0 ]
 then
-    echo "connect to google.com successed"
+    echo "Successfully connected to Google, no need to use Go proxy"
     CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o server .
 else
-    echo "connect to google.com failed"
+    echo "Google is blocked, Go proxy is enabled: GOPROXY=https://goproxy.cn,direct"
     CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GOPROXY=https://goproxy.cn,direct go build -ldflags="-w -s" -o server .
 fi
