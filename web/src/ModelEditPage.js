097e3267790b24818f6c7d8fbb57b64ccb265a3c
Yixiang Zhao <seriouszyx@foxmail.com>
2022-05-24 12:47:03 +0800

    translation and indentation
    Signed-off-by: Yixiang Zhao <seriouszyx@foxmail.com>

--- web/src/ModelEditPage.js
+++ web/src/ModelEditPage.js
@@ -23,186 +23,186 @@ import TextArea from "antd/es/input/TextArea";
 const { Option } = Select;
 
 class ModelEditPage extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			classes: props,
-			organizationName: props.organizationName !== undefined ? props.organizationName : props.match.params.organizationName,
-			modelName: props.match.params.modelName,
-			model: null,
-			organizations: [],
-			users: [],
-			models: [],
-			mode: props.location.mode !== undefined ? props.location.mode : "edit",
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      classes: props,
+      organizationName: props.organizationName !== undefined ? props.organizationName : props.match.params.organizationName,
+      modelName: props.match.params.modelName,
+      model: null,
+      organizations: [],
+      users: [],
+      models: [],
+      mode: props.location.mode !== undefined ? props.location.mode : "edit",
+    };
+  }
 
-	UNSAFE_componentWillMount() {
-		this.getModel();
-		this.getOrganizations();
-	}
+  UNSAFE_componentWillMount() {
+    this.getModel();
+    this.getOrganizations();
+  }
 
-	getModel() {
-		ModelBackend.getModel(this.state.organizationName, this.state.modelName)
-			.then((model) => {
-				this.setState({
-					model: model,
-				});
+  getModel() {
+    ModelBackend.getModel(this.state.organizationName, this.state.modelName)
+      .then((model) => {
+        this.setState({
+          model: model,
+        });
 
-				this.getModels(model.owner);
-			});
-	}
+        this.getModels(model.owner);
+      });
+  }
 
-	getOrganizations() {
-		OrganizationBackend.getOrganizations("admin")
-			.then((res) => {
-				this.setState({
-					organizations: (res.msg === undefined) ? res : [],
-				});
-			});
-	}
+  getOrganizations() {
+    OrganizationBackend.getOrganizations("admin")
+      .then((res) => {
+        this.setState({
+          organizations: (res.msg === undefined) ? res : [],
+        });
+      });
+  }
 
-	getModels(organizationName) {
-		ModelBackend.getModels(organizationName)
-			.then((res) => {
-				this.setState({
-					models: res,
-				});
-			});
-	}
+  getModels(organizationName) {
+    ModelBackend.getModels(organizationName)
+      .then((res) => {
+        this.setState({
+          models: res,
+        });
+      });
+  }
 
-	parseModelField(key, value) {
-		if ([""].includes(key)) {
-			value = Setting.myParseInt(value);
-		}
-		return value;
-	}
+  parseModelField(key, value) {
+    if ([""].includes(key)) {
+      value = Setting.myParseInt(value);
+    }
+    return value;
+  }
 
-	updateModelField(key, value) {
-		value = this.parseModelField(key, value);
+  updateModelField(key, value) {
+    value = this.parseModelField(key, value);
 
-		let model = this.state.model;
-		model[key] = value;
-		this.setState({
-			model: model,
-		});
-	}
+    let model = this.state.model;
+    model[key] = value;
+    this.setState({
+      model: model,
+    });
+  }
 
-	renderModel() {
-		return (
-			<Card size="small" title={
-				<div>
-					{this.state.mode === "add" ? i18next.t("model:New Model") : i18next.t("model:Edit Model")}&nbsp;&nbsp;&nbsp;&nbsp;
-					<Button onClick={() => this.submitModelEdit(false)}>{i18next.t("general:Save")}</Button>
-					<Button style={{marginLeft: '20px'}} type="primary" onClick={() => this.submitModelEdit(true)}>{i18next.t("general:Save & Exit")}</Button>
-					{this.state.mode === "add" ? <Button style={{marginLeft: '20px'}} onClick={() => this.deleteModel()}>{i18next.t("general:Cancel")}</Button> : null}
-				</div>
-			} style={(Setting.isMobile())? {margin: '5px'}:{}} type="inner">
-				<Row style={{marginTop: '10px'}} >
-					<Col style={{marginTop: '5px'}} span={(Setting.isMobile()) ? 22 : 2}>
-						{Setting.getLabel(i18next.t("general:Organization"), i18next.t("general:Organization - Tooltip"))} :
-					</Col>
-					<Col span={22} >
-						<Select virtual={false} style={{width: '100%'}} value={this.state.model.owner} onChange={(value => {this.updateModelField('owner', value);})}>
-							{
-								this.state.organizations.map((organization, index) => <Option key={index} value={organization.name}>{organization.name}</Option>)
-							}
-						</Select>
-					</Col>
-				</Row>
-				<Row style={{marginTop: '20px'}} >
-					<Col style={{marginTop: '5px'}} span={(Setting.isMobile()) ? 22 : 2}>
-						{Setting.getLabel(i18next.t("general:Name"), i18next.t("general:Name - Tooltip"))} :
-					</Col>
-					<Col span={22} >
-						<Input value={this.state.model.name} onChange={e => {
-							this.updateModelField('name', e.target.value);
-						}} />
-					</Col>
-				</Row>
-				<Row style={{marginTop: '20px'}} >
-					<Col style={{marginTop: '5px'}} span={(Setting.isMobile()) ? 22 : 2}>
-						{Setting.getLabel(i18next.t("general:Display name"), i18next.t("general:Display name - Tooltip"))} :
-					</Col>
-					<Col span={22} >
-						<Input value={this.state.model.displayName} onChange={e => {
-							this.updateModelField('displayName', e.target.value);
-						}} />
-					</Col>
-				</Row>
-				<Row style={{marginTop: '20px'}} >
-					<Col style={{marginTop: '5px'}} span={(Setting.isMobile()) ? 22 : 2}>
-						{Setting.getLabel(i18next.t("model:Model"), i18next.t("model:Model - Tooltip"))} :
-					</Col>
-					<Col span={22}>
-						<TextArea rows={10} value={this.state.model.modelText} onChange={e => {
-							this.updateModelField('modelText', e.target.value);
-						}} />
-					</Col>
-				</Row>
-				<Row style={{marginTop: '20px'}} >
-					<Col style={{marginTop: '5px'}} span={(Setting.isMobile()) ? 19 : 2}>
-						{Setting.getLabel(i18next.t("general:Is enabled"), i18next.t("general:Is enabled - Tooltip"))} :
-					</Col>
-					<Col span={1} >
-						<Switch checked={this.state.model.isEnabled} onChange={checked => {
-							this.updateModelField('isEnabled', checked);
-						}} />
-					</Col>
-				</Row>
-			</Card>
-		)
-	}
+  renderModel() {
+    return (
+      <Card size="small" title={
+        <div>
+          {this.state.mode === "add" ? i18next.t("model:New Model") : i18next.t("model:Edit Model")}&nbsp;&nbsp;&nbsp;&nbsp;
+          <Button onClick={() => this.submitModelEdit(false)}>{i18next.t("general:Save")}</Button>
+          <Button style={{marginLeft: '20px'}} type="primary" onClick={() => this.submitModelEdit(true)}>{i18next.t("general:Save & Exit")}</Button>
+          {this.state.mode === "add" ? <Button style={{marginLeft: '20px'}} onClick={() => this.deleteModel()}>{i18next.t("general:Cancel")}</Button> : null}
+        </div>
+      } style={(Setting.isMobile())? {margin: '5px'}:{}} type="inner">
+        <Row style={{marginTop: '10px'}} >
+          <Col style={{marginTop: '5px'}} span={(Setting.isMobile()) ? 22 : 2}>
+            {Setting.getLabel(i18next.t("general:Organization"), i18next.t("general:Organization - Tooltip"))} :
+          </Col>
+          <Col span={22} >
+            <Select virtual={false} style={{width: '100%'}} value={this.state.model.owner} onChange={(value => {this.updateModelField('owner', value);})}>
+              {
+                this.state.organizations.map((organization, index) => <Option key={index} value={organization.name}>{organization.name}</Option>)
+              }
+            </Select>
+          </Col>
+        </Row>
+        <Row style={{marginTop: '20px'}} >
+          <Col style={{marginTop: '5px'}} span={(Setting.isMobile()) ? 22 : 2}>
+            {Setting.getLabel(i18next.t("general:Name"), i18next.t("general:Name - Tooltip"))} :
+          </Col>
+          <Col span={22} >
+            <Input value={this.state.model.name} onChange={e => {
+              this.updateModelField('name', e.target.value);
+            }} />
+          </Col>
+        </Row>
+        <Row style={{marginTop: '20px'}} >
+          <Col style={{marginTop: '5px'}} span={(Setting.isMobile()) ? 22 : 2}>
+            {Setting.getLabel(i18next.t("general:Display name"), i18next.t("general:Display name - Tooltip"))} :
+          </Col>
+          <Col span={22} >
+            <Input value={this.state.model.displayName} onChange={e => {
+              this.updateModelField('displayName', e.target.value);
+            }} />
+          </Col>
+        </Row>
+        <Row style={{marginTop: '20px'}} >
+          <Col style={{marginTop: '5px'}} span={(Setting.isMobile()) ? 22 : 2}>
+            {Setting.getLabel(i18next.t("model:Model"), i18next.t("model:Model - Tooltip"))} :
+          </Col>
+          <Col span={22}>
+            <TextArea rows={10} value={this.state.model.modelText} onChange={e => {
+              this.updateModelField('modelText', e.target.value);
+            }} />
+          </Col>
+        </Row>
+        <Row style={{marginTop: '20px'}} >
+          <Col style={{marginTop: '5px'}} span={(Setting.isMobile()) ? 19 : 2}>
+            {Setting.getLabel(i18next.t("general:Is enabled"), i18next.t("general:Is enabled - Tooltip"))} :
+          </Col>
+          <Col span={1} >
+            <Switch checked={this.state.model.isEnabled} onChange={checked => {
+              this.updateModelField('isEnabled', checked);
+            }} />
+          </Col>
+        </Row>
+      </Card>
+    )
+  }
 
-	submitModelEdit(willExist) {
-		let model = Setting.deepCopy(this.state.model);
-		ModelBackend.updateModel(this.state.organizationName, this.state.modelName, model)
-			.then((res) => {
-				if (res.msg === "") {
-					Setting.showMessage("success", `Successfully saved`);
-					this.setState({
-						modelName: this.state.model.name,
-					});
+  submitModelEdit(willExist) {
+    let model = Setting.deepCopy(this.state.model);
+    ModelBackend.updateModel(this.state.organizationName, this.state.modelName, model)
+      .then((res) => {
+        if (res.msg === "") {
+          Setting.showMessage("success", `Successfully saved`);
+          this.setState({
+            modelName: this.state.model.name,
+          });
 
-					if (willExist) {
-						this.props.history.push(`/models`);
-					} else {
-						this.props.history.push(`/models/${this.state.model.owner}/${this.state.model.name}`);
-					}
-				} else {
-					Setting.showMessage("error", res.msg);
-					this.updateModelField('name', this.state.modelName);
-				}
-			})
-			.catch(error => {
-				Setting.showMessage("error", `Failed to connect to server: ${error}`);
-			});
-	}
+          if (willExist) {
+            this.props.history.push(`/models`);
+          } else {
+            this.props.history.push(`/models/${this.state.model.owner}/${this.state.model.name}`);
+          }
+        } else {
+          Setting.showMessage("error", res.msg);
+          this.updateModelField('name', this.state.modelName);
+        }
+      })
+      .catch(error => {
+        Setting.showMessage("error", `Failed to connect to server: ${error}`);
+      });
+  }
 
-	deleteModel() {
-		ModelBackend.deleteModel(this.state.model)
-			.then(() => {
-				this.props.history.push(`/models`);
-			})
-			.catch(error => {
-				Setting.showMessage("error", `Model failed to delete: ${error}`);
-			});
-	}
+  deleteModel() {
+    ModelBackend.deleteModel(this.state.model)
+      .then(() => {
+        this.props.history.push(`/models`);
+      })
+      .catch(error => {
+        Setting.showMessage("error", `Model failed to delete: ${error}`);
+      });
+  }
 
-	render() {
-		return (
-			<div>
-				{
-					this.state.model !== null ? this.renderModel() : null
-				}
-				<div style={{marginTop: '20px', marginLeft: '40px'}}>
-					<Button size="large" onClick={() => this.submitModelEdit(false)}>{i18next.t("general:Save")}</Button>
-					<Button style={{marginLeft: '20px'}} type="primary" size="large" onClick={() => this.submitModelEdit(true)}>{i18next.t("general:Save & Exit")}</Button>
-					{this.state.mode === "add" ? <Button style={{marginLeft: '20px'}} size="large" onClick={() => this.deleteModel()}>{i18next.t("general:Cancel")}</Button> : null}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        {
+          this.state.model !== null ? this.renderModel() : null
+        }
+        <div style={{marginTop: '20px', marginLeft: '40px'}}>
+          <Button size="large" onClick={() => this.submitModelEdit(false)}>{i18next.t("general:Save")}</Button>
+          <Button style={{marginLeft: '20px'}} type="primary" size="large" onClick={() => this.submitModelEdit(true)}>{i18next.t("general:Save & Exit")}</Button>
+          {this.state.mode === "add" ? <Button style={{marginLeft: '20px'}} size="large" onClick={() => this.deleteModel()}>{i18next.t("general:Cancel")}</Button> : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ModelEditPage;
--- web/src/backend/ModelBackend.js
+++ web/src/backend/ModelBackend.js
@@ -15,42 +15,42 @@
 import * as Setting from "../Setting";
 
 export function getModels(owner, page = "", pageSize = "", field = "", value = "", sortField = "", sortOrder = "") {
-	return fetch(`${Setting.ServerUrl}/api/get-models?owner=${owner}&p=${page}&pageSize=${pageSize}&field=${field}&value=${value}&sortField=${sortField}&sortOrder=${sortOrder}`, {
-		method: "GET",
-		credentials: "include"
-	}).then(res => res.json());
+  return fetch(`${Setting.ServerUrl}/api/get-models?owner=${owner}&p=${page}&pageSize=${pageSize}&field=${field}&value=${value}&sortField=${sortField}&sortOrder=${sortOrder}`, {
+    method: "GET",
+    credentials: "include"
+  }).then(res => res.json());
 }
 
 export function getModel(owner, name) {
-	return fetch(`${Setting.ServerUrl}/api/get-model?id=${owner}/${encodeURIComponent(name)}`, {
-		method: "GET",
-		credentials: "include"
-	}).then(res => res.json());
+  return fetch(`${Setting.ServerUrl}/api/get-model?id=${owner}/${encodeURIComponent(name)}`, {
+    method: "GET",
+    credentials: "include"
+  }).then(res => res.json());
 }
 
 export function updateModel(owner, name, model) {
-	let newModel = Setting.deepCopy(model);
-	return fetch(`${Setting.ServerUrl}/api/update-model?id=${owner}/${encodeURIComponent(name)}`, {
-		method: 'POST',
-		credentials: 'include',
-		body: JSON.stringify(newModel),
-	}).then(res => res.json());
+  let newModel = Setting.deepCopy(model);
+  return fetch(`${Setting.ServerUrl}/api/update-model?id=${owner}/${encodeURIComponent(name)}`, {
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify(newModel),
+  }).then(res => res.json());
 }
 
 export function addModel(model) {
-	let newModel = Setting.deepCopy(model);
-	return fetch(`${Setting.ServerUrl}/api/add-model`, {
-		method: 'POST',
-		credentials: 'include',
-		body: JSON.stringify(newModel),
-	}).then(res => res.json());
+  let newModel = Setting.deepCopy(model);
+  return fetch(`${Setting.ServerUrl}/api/add-model`, {
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify(newModel),
+  }).then(res => res.json());
 }
 
 export function deleteModel(model) {
-	let newModel = Setting.deepCopy(model);
-	return fetch(`${Setting.ServerUrl}/api/delete-model`, {
-		method: 'POST',
-		credentials: 'include',
-		body: JSON.stringify(newModel),
-	}).then(res => res.json());
+  let newModel = Setting.deepCopy(model);
+  return fetch(`${Setting.ServerUrl}/api/delete-model`, {
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify(newModel),
+  }).then(res => res.json());
 }
