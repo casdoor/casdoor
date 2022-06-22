16f5569e5067db2f59fddbb2047173d910be0234
Yixiang Zhao <seriouszyx@foxmail.com>
2022-06-22 22:30:27 +0800

    fix: encryption without salt (#821)
    Signed-off-by: Yixiang Zhao <seriouszyx@foxmail.com>

--- cred/sha256-salt_test.go
+++ cred/sha256-salt_test.go
@@ -25,3 +25,10 @@ func TestGetSaltedPassword(t *testing.T) {
 	cm := NewSha256SaltCredManager()
 	fmt.Printf("%s -> %s\n", password, cm.GetHashedPassword(password, "", salt))
 }
+
+func TestGetPassword(t *testing.T) {
+	password := "123456"
+	cm := NewSha256SaltCredManager()
+	// https://passwordsgenerator.net/sha256-hash-generator/
+	fmt.Printf("%s -> %s\n", "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", cm.GetHashedPassword(password, "", ""))
+}
