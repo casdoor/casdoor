43e474839c48e4cc2ddf59d6e53c01388a876665
atompi <atompi@foxmail.com>
2022-06-27 18:01:53 +0800

    update docker-entrypoint.sh: remove useless environment variable judgments.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,6 @@
 
 service mariadb start
 
-if [ "${MYSQL_ROOT_PASSWORD}" = "" ]; then
-    MYSQL_ROOT_PASSWORD=123456
-fi
-
 mysqladmin -u root password ${MYSQL_ROOT_PASSWORD}
 
 exec /app/server --createDatabase=true
