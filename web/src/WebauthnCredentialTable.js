ad7ac2d82194d0e621bfcf3e6a5edfb3d14cbdd8
Gucheng <85475922+nomeguy@users.noreply.github.com>
2022-07-12 19:57:57 +0800

    Update WebauthnCredentialTable.js

--- web/src/WebauthnCredentialTable.js
+++ web/src/WebauthnCredentialTable.js
@@ -1,3 +1,17 @@
+// Copyright 2022 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import React from "react";
 import {Button, Table} from 'antd';
 import i18next from "i18next";
@@ -5,9 +19,7 @@ import * as UserWebauthnBackend from "./backend/UserWebauthnBackend";
 import * as Setting from "./Setting";
 
 class WebAuthnCredentialTable extends React.Component {
-
   render() {
-    const datasource = this.props.table
     const columns = [
       {
         title: i18next.t("user:WebAuthn credentials"),
@@ -18,14 +30,16 @@ class WebAuthnCredentialTable extends React.Component {
         title: i18next.t("general:Action"),
         key: 'action',
         render: (text, record, index) => {
-          return (<Button style={{marginTop: '5px', marginBottom: '5px', marginRight: '5px'}} type="danger" onClick={() => {
-            this.deleteRow(this.props.table, index)
-          }}>{i18next.t("general:Delete")}</Button>)
+          return (
+            <Button style={{marginTop: '5px', marginBottom: '5px', marginRight: '5px'}} type="danger" onClick={() => {this.deleteRow(this.props.table, index)}}>
+              {i18next.t("general:Delete")}
+            </Button>
+          )
         }
       }
     ]
     return (
-      <Table scroll={{x: 'max-content'}} rowKey={record => record.ID} columns={columns} dataSource={datasource} size="middle" bordered pagination={false}
+      <Table scroll={{x: 'max-content'}} rowKey={"ID"} columns={columns} dataSource={this.props.table} size="middle" bordered pagination={false}
         title={() => (
           <div>
             {i18next.t("user:WebAuthn credentials")}&nbsp;&nbsp;&nbsp;&nbsp;
@@ -40,7 +54,6 @@ class WebAuthnCredentialTable extends React.Component {
     this.props.updateTable(table);
   }
 
-
   registerWebAuthn() {
     UserWebauthnBackend.registerWebauthnCredential().then((res) => {
       if (res.msg === "") {
@@ -48,7 +61,8 @@ class WebAuthnCredentialTable extends React.Component {
       } else {
         Setting.showMessage("error", res.msg);
       }
-      this.props.refresh()
+
+      this.props.refresh();
     }).catch(error => {
       Setting.showMessage("error", `Failed to connect to server: ${error}`);
     });
