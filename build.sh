6b4ee4ef5e46f3aee21924f52b7cbea03676197f
huanglijie <486438441@qq.com>
2022-08-31 15:12:48 +0800

    fix: refactor build.sh #1081

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ curl www.google.com -o /dev/null --connect-timeout 5 2 > /dev/null
 if [ $? == 0 ]
 then
     echo "Successfully connected to Google, no need to use Go proxy"
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o server .
 else
     echo "Google is blocked, Go proxy is enabled: GOPROXY=https://goproxy.cn,direct"
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GOPROXY=https://goproxy.cn,direct go build -ldflags="-w -s" -o server .
+    GOPROXY=GOPROXY=https://goproxy.cn,direct
 fi
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 $GOPROXY go build -ldflags="-w -s" -o server .
