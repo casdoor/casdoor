b0ab683455dadea99ce63dd144b4298f50ec9539
imp2002 <imp07@qq.com>
2022-09-04 11:13:40 +0800

    feat: adjust the top menu bar width

--- web/src/App.js
+++ web/src/App.js
@@ -601,7 +601,7 @@ class App extends Component {
                   // theme="dark"
                   mode={(Setting.isMobile() && this.isStartPages()) ? "inline" : "horizontal"}
                   selectedKeys={[`${this.state.selectedMenuKey}`]}
-                  style={{lineHeight: "64px", width: "80%", position: "absolute", left: "145px"}}
+                  style={{lineHeight: "64px", width: "75%", position: "absolute", left: "145px"}}
                 >
                   {
                     this.renderMenu()
