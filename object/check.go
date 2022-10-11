7308d8489bd3449dcc83b28f712c3ecd5591badb
Yang Luo <hsluoyz@qq.com>
2022-10-11 19:33:50 +0800

    Update check.go

--- object/check.go
+++ object/check.go
@@ -324,9 +324,7 @@ func CheckUsername(name string) string {
 	// https://stackoverflow.com/questions/58726546/github-username-convention-using-regex
 	re, _ := regexp.Compile("^[a-zA-Z0-9]+((?:-[a-zA-Z0-9]+)|(?:_[a-zA-Z0-9]+))*$")
 	if !re.MatchString(name) {
-		return fmt.Sprintf("The name '%s' may only contain alphanumeric characters, underline or hyphens, "+
-			"cannot have multiple consecutive hyphens, "+
-			"and cannot begin or end with a hyphen or underline.", name)
+		return "The username may only contain alphanumeric characters, underlines or hyphens, cannot have consecutive hyphens or underlines, and cannot begin or end with a hyphen or underline."
 	}
 
 	return ""
