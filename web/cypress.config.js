beae0a0f11ecaafed44b3b19505f5cf7cefe6f08
jakiuncle <2509913103@qq.com>
2023-02-03 15:45:03 +0800

    feat: add Test Retries

--- web/cypress.config.js
+++ web/cypress.config.js
@@ -1,7 +1,10 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    "retries": {
+      "runMode": 2,
+      "openMode": 0
+    }
   },
-};
+});
