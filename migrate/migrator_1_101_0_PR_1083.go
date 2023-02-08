30506e8860fcb953608b0f8ec4647588ab6ecfca
Zayn Xie <84443886+xiaoniuren99@users.noreply.github.com>
2023-02-08 23:32:31 +0800

    feat: integrate application session management into Casdoor's session management (#774)

--- migrate/migrator_1_101_0_PR_1083.go
+++ migrate/migrator_1_101_0_PR_1083.go
@@ -66,10 +66,7 @@ func (*Migrator_1_101_0_PR_1083) IsMigrationNeeded(adapter *object.Adapter) bool
 	exist2, _ := adapter.Engine.IsTableExist("permission")
 	exist3, _ := adapter.Engine.IsTableExist("permission_rule")
 
-	err1 := adapter.Engine.Table("model").Find(&[]*oldModel{})
-	err2 := adapter.Engine.Table("permission").Find(&[]*oldPermission{})
-
-	if exist1 && exist2 && exist3 && err1 == nil && err2 == nil {
+	if exist1 && exist2 && exist3 {
 		return true
 	}
 	return false
