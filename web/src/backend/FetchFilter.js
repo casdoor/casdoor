842d4865b2bceacfbca034a46a6c9842bd737861
Yang Luo <hsluoyz@qq.com>
2023-02-18 16:11:23 +0800

    Fix router error message's i18n

--- web/src/backend/FetchFilter.js
+++ web/src/backend/FetchFilter.js
@@ -31,7 +31,7 @@ const demoModeCallback = (res) => {
         okText: i18next.t("user:OK"),
         cancelText: i18next.t("general:Cancel"),
         onOk() {
-          Setting.openLink(`https://demo.casdoor.com${location.path}${location.search}?username=built-in/admin&password=123`);
+          Setting.openLink(`https://demo.casdoor.com${location.pathname}${location.search}?username=built-in/admin&password=123`);
         },
         onCancel() {},
       });
