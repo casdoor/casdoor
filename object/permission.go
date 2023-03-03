a1dabe64ca3409ed2255598f0dc9f353184e3bef
hsluoyz <hsluoyz@qq.com>
2023-03-03 17:56:30 +0800

    Update permission.go

--- object/permission.go
+++ object/permission.go
@@ -244,8 +244,9 @@ func GetPermissionsByUser(userId string) []*Permission {
 	if err != nil {
 		panic(err)
 	}
-	for idx := range permissions {
-		permissions[idx].Users = nil
+
+	for i := range permissions {
+		permissions[i].Users = nil
 	}
 
 	return permissions
