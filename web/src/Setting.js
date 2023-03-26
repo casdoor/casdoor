a5b2a918506fa00ecf0f9a0fcbb47385a62bdcf5
hsluoyz <hsluoyz@qq.com>
2023-03-26 23:46:30 +0800

    Update Setting.js

--- web/src/Setting.js
+++ web/src/Setting.js
@@ -704,7 +704,7 @@ export function getProviderLogoURL(provider) {
     if (provider.type === "Custom") {
       return provider.customLogo;
     }
-    return `${StaticBaseUrl}/buttons/${provider.type.toLowerCase()}.svg`;
+    return `${StaticBaseUrl}/img/social_${provider.type.toLowerCase()}.png`;
   } else {
     const info = OtherProviderInfo[provider.category][provider.type];
     // avoid crash when provider is not found
