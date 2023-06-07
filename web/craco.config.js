fec904b078778a1b7160266804d138ed6370e555
chenjiawei <907997375@qq.com>
2023-06-07 16:01:38 +0800

    feat：add webpack configuration to split npm packages and extract public code in main.js

--- web/craco.config.js
+++ web/craco.config.js
@@ -1,6 +1,40 @@
 const CracoLessPlugin = require("craco-less");
+const path = require("path")
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      if(webpackConfig.mode === "production") {
+        webpackConfig.devtool = false;
+        webpackConfig.output = {
+          path: path.resolve(__dirname, './build'),
+          filename: 'static/js/[name].[contenthash].js',
+          chunkFilename: 'static/js/[name].[chunkhash].chunk.js'
+        };
+        webpackConfig.optimization = {
+          splitChunks: {
+            minSize: 30,
+            cacheGroups: {
+              default: {
+                name: 'common',
+                chunks: 'initial',
+                minChunks: 2,
+                priority: -20
+              },
+              vendors: {
+                test: /[\\/]node_modules[\\/]/,
+                name: 'vendor',
+                chunks: 'initial',
+                priority: -10
+              },
+            }
+          }
+        }
+      }
+      return webpackConfig;
+    }
+  },
+
   devServer: {
     proxy: {
       "/api": {
