1a13103b412b888aedf2c97c4ee30821b2a02261
XDTD <1355582364@qq.com>
2023-06-21 11:00:13 +0800

    fix: fix user information sync issue

--- object/syncer_user.go
+++ object/syncer_user.go
@@ -123,8 +123,7 @@ func (syncer *Syncer) updateUser(user *OriginalUser) (bool, error) {
 
 func (syncer *Syncer) updateUserForOriginalFields(user *User) (bool, error) {
 	var err error
-	owner, name := util.GetOwnerAndNameFromId(user.GetId())
-	oldUser, err := getUserById(owner, name)
+	oldUser, err := getUserById(user.Owner, user.Id)
 	if oldUser == nil || err != nil {
 		return false, err
 	}
