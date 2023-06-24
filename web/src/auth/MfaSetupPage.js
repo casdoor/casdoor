54c170d5f5c3a6c57ff0c76e5effa9e702d333bb
magicwind <2814461814@qq.com>
2023-06-24 10:48:24 +0800

    feat:i18n

--- web/src/auth/MfaSetupPage.js
+++ web/src/auth/MfaSetupPage.js
@@ -250,7 +250,7 @@ class MfaSetupPage extends React.Component {
                   mfaType: EmailMfaType,
                 });
               }
-              }>{i18next.t("mfa:Use email")}</Button>
+              }>{i18next.t("mfa:Use Email")}</Button>
             }
             {
               (this.state.mfaType === SmsMfaType || this.props.account.mfaPhoneEnabled) ? null :
@@ -268,7 +268,7 @@ class MfaSetupPage extends React.Component {
                     mfaType: TotpMfaType,
                   });
                 }
-                }>{i18next.t("mfa:Use authenticator app")}</Button>
+                }>{i18next.t("mfa:Use Authenticator App")}</Button>
             }
           </Col>
         </div>
