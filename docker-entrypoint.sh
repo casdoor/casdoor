48bab26e1498972c96a29e09cde5c48efc78060a
Alex OvsInc <ovsinc@ya.ru>
2023-07-05 16:16:38 +0300

    Minor improvements for local casdoor launch

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,9 @@ if [ "${MYSQL_ROOT_PASSWORD}" = "" ] ;then MYSQL_ROOT_PASSWORD=123456 ;fi
 
 service mariadb start
 
-mysqladmin -u root password ${MYSQL_ROOT_PASSWORD}
+mysqladmin -u root password "${MYSQL_ROOT_PASSWORD}"
 
-exec /server --createDatabase=true
+_create_db="true"
+[[ -n "$NO_CREATE_DATABASE" ]] && _create_db="false"
+
+exec /server --createDatabase="$_create_db"
