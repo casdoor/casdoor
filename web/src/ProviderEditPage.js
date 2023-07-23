dc0f8aae27d9a88b1aa1630862da7ff509c7d6ea
UsherFall <1270329076@qq.com>
2023-07-23 13:05:37 +0800

    feat: hide field of minio storage provider

--- web/src/ProviderEditPage.js
+++ web/src/ProviderEditPage.js
@@ -628,7 +628,7 @@ class ProviderEditPage extends React.Component {
                 </Col>
               </Row>
             )}
-            {["Local File System"].includes(this.state.provider.type) ? null : (
+            {["Local File System", "MinIO"].includes(this.state.provider.type) ? null : (
               <Row style={{marginTop: "20px"}} >
                 <Col style={{marginTop: "5px"}} span={2}>
                   {Setting.getLabel(i18next.t("provider:Endpoint (Intranet)"), i18next.t("provider:Region endpoint for Intranet"))} :
@@ -672,7 +672,7 @@ class ProviderEditPage extends React.Component {
                 }} />
               </Col>
             </Row>
-            {["AWS S3", "MinIO", "Tencent Cloud COS"].includes(this.state.provider.type) ? (
+            {["AWS S3", "Tencent Cloud COS"].includes(this.state.provider.type) ? (
               <Row style={{marginTop: "20px"}} >
                 <Col style={{marginTop: "5px"}} span={2}>
                   {Setting.getLabel(i18next.t("provider:Region ID"), i18next.t("provider:Region ID - Tooltip"))} :
