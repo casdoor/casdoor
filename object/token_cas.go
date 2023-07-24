8c8d04bcff843046087081a294cc858cf3de2bab
hsluoyz <hsluoyz@qq.com>
2023-07-24 11:39:42 +0800

    Update token_cas.go

--- object/token_cas.go
+++ object/token_cas.go
@@ -123,11 +123,11 @@ var stToServiceResponse sync.Map
 // pgt is short for proxy granting ticket
 var pgtToServiceResponse sync.Map
 
-func CheckCasRestrict(application *Application, lang string, service string) (err error) {
+func CheckCasRestrict(application *Application, lang string, service string) error {
 	if len(application.RedirectUris) > 0 && !application.IsRedirectUriValid(service) {
 		return fmt.Errorf(i18n.Translate(lang, "token:Redirect URI: %s doesn't exist in the allowed Redirect URI list"), service)
 	}
-	return
+	return nil
 }
 
 func StoreCasTokenForPgt(token *CasAuthenticationSuccess, service, userId string) string {
