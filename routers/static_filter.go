01e58158b70bbb5e312c5d1a3e59c4b18587cdbf
Yang Luo <hsluoyz@qq.com>
2023-08-08 19:16:55 +0800

    feat: Remove useless code

--- routers/static_filter.go
+++ routers/static_filter.go
@@ -55,12 +55,6 @@ func StaticFilter(ctx *context.Context) {
 		path += urlPath
 	}
 
-	path2 := strings.TrimPrefix(path, "web/build/images/")
-	if util.FileExist(path2) {
-		makeGzipResponse(ctx.ResponseWriter, ctx.Request, path2)
-		return
-	}
-
 	if !util.FileExist(path) {
 		path = "web/build/index.html"
 	}
