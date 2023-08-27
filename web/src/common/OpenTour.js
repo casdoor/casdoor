7a85b745734747421fba5e4c2686fa6a7301568e
Baihhh <63110802+Baihhh@users.noreply.github.com>
2023-08-27 23:18:14 +0800

    fix: fix tour disabled state (#2264)
    * fix: distinguish between pages that can tour or not
    
    * Update OpenTour.js
    
    ---------
    
    Co-authored-by: hsluoyz <hsluoyz@qq.com>

--- web/src/common/OpenTour.js
+++ web/src/common/OpenTour.js
@@ -26,13 +26,23 @@ class OpenTour extends React.Component {
     };
   }
 
+  canTour = () => {
+    const path = window.location.pathname.replace("/", "");
+    return TourConfig.TourUrlList.indexOf(path) !== -1 || path === "";
+  };
+
   render() {
     return (
-      <Tooltip title="Click to enable the help wizard">
-        <div className="select-box" style={{display: Setting.isMobile() ? "none" : null, ...this.props.style}} onClick={() => TourConfig.setIsTourVisible(true)} >
-          <QuestionCircleOutlined style={{fontSize: "24px", color: "#4d4d4d"}} />
+      this.canTour() ?
+        <Tooltip title="Click to enable the help wizard.">
+          <div className="select-box" style={{display: Setting.isMobile() ? "none" : null, ...this.props.style}} onClick={() => TourConfig.setIsTourVisible(true)} >
+            <QuestionCircleOutlined style={{fontSize: "24px", color: "#4d4d4d"}} />
+          </div>
+        </Tooltip>
+        :
+        <div className="select-box" style={{display: Setting.isMobile() ? "none" : null, cursor: "not-allowed", ...this.props.style}} >
+          <QuestionCircleOutlined style={{fontSize: "24px", color: "#adadad"}} />
         </div>
-      </Tooltip>
     );
   }
 }
