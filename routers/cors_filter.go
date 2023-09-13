a12ba7fb85f016883d5e79c9b2a347d550c5d411
Yaodong Yu <2814461814@qq.com>
2023-09-13 18:11:13 +0800

    feat: allow CORS for UserInfo API in OIDC (#2313)

--- routers/cors_filter.go
+++ routers/cors_filter.go
@@ -40,6 +40,13 @@ func CorsFilter(ctx *context.Context) {
 		return
 	}
 
+	if ctx.Request.RequestURI == "/api/userinfo" {
+		ctx.Output.Header(headerAllowOrigin, origin)
+		ctx.Output.Header(headerAllowMethods, "POST, GET, OPTIONS, DELETE")
+		ctx.Output.Header(headerAllowHeaders, "Content-Type, Authorization")
+		return
+	}
+
 	if origin != "" && originConf != "" && origin != originConf {
 		ok, err := object.IsOriginAllowed(origin)
 		if err != nil {
