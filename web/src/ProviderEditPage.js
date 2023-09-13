24b1f50d77bac1bf953c67caa4913beaee9394a0
haiwu <likamylike@gmail.com>
2023-09-13 15:04:57 +0800

    feat: update root cert

--- web/src/ProviderEditPage.js
+++ web/src/ProviderEditPage.js
@@ -1115,7 +1115,7 @@ class ProviderEditPage extends React.Component {
           (this.state.provider.type === "Alipay") ? (
             <Row style={{marginTop: "20px"}} >
               <Col style={{marginTop: "5px"}} span={(Setting.isMobile()) ? 22 : 2}>
-                {Setting.getLabel(i18next.t("general:Alipay Cert"), i18next.t("general:Alipay Cert - Tooltip"))} :
+                {Setting.getLabel(i18next.t("general:Root Cert"), i18next.t("general:Root Cert - Tooltip"))} :
               </Col>
               <Col span={22} >
                 <Select virtual={false} style={{width: "100%"}} value={this.state.provider.metadata} onChange={(value => {this.updateProviderField("metadata", value);})}>
