1fb6d30a0eb980688ca0b968ec6782d052c0da08
YunShu <im@yunshu.site>
2023-10-10 21:14:12 +0800

    ci: stop building docker image of linux/arm64

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ else
     export GOPROXY="https://goproxy.cn,direct"
 fi
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o server_linux_amd64 .
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-w -s" -o server_linux_arm64 .
+# CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-w -s" -o server_linux_arm64 .
