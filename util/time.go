1055d7781b2773a9c39292c23b6805550d1bd7ad
Yang Luo <hsluoyz@qq.com>
2023-10-15 12:43:36 +0800

    Improve error handling in AutoSigninFilter

--- util/time.go
+++ util/time.go
@@ -58,8 +58,10 @@ func Time2String(timestamp time.Time) string {
 	return timestamp.Format(time.RFC3339)
 }
 
-func IsTokenExpired(createdTime string, expiresIn int) bool {
+func IsTokenExpired(createdTime string, expiresIn int) (bool, string) {
 	createdTimeObj, _ := time.Parse(time.RFC3339, createdTime)
 	expiresAtObj := createdTimeObj.Add(time.Duration(expiresIn) * time.Second)
-	return time.Now().After(expiresAtObj)
+	isExpired := time.Now().After(expiresAtObj)
+	expireTime := expiresAtObj.Local().Format(time.RFC3339)
+	return isExpired, expireTime
 }
