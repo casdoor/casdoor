7d4652c70ef86c80ab146583eb3317dac2234d3f
Rishi Singh <rs3949427@gmail.com>
2023-10-20 13:41:48 +0530

    #1081 Issue: Refactor build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#try to connect to google to determine whether user need to use proxy
+# Try to connect to google to determine whether user needs to use proxy
 curl www.google.com -o /dev/null --connect-timeout 5 2> /dev/null
-if [ $? == 0 ]
-then
+if [ $? == 0 ]; then
     echo "Successfully connected to Google, no need to use Go proxy"
 else
     echo "Google is blocked, Go proxy is enabled: GOPROXY=https://goproxy.cn,direct"
