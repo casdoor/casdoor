d66d9907584185dd0ac562952f0df8293b043678
Jens-Uwe Mager <jum@anubis.han.de>
2023-12-08 22:48:40 +0100

    Integrate the GOPROXY check into the build script.

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-set -e -o pipefail
 DOCKER_PREFIX=jumager/casdoor
-#branch=$(basename $(git rev-parse --abbrev-ref HEAD))
-branch=latest
-docker buildx build --platform linux/arm64,linux/amd64 --target STANDARD -t ${DOCKER_PREFIX}:${branch} -f Dockerfile --push .
+branch=$(basename $(git rev-parse --abbrev-ref HEAD))
+#try to connect to google to determine whether user need to use proxy
+curl www.google.com -o /dev/null --connect-timeout 5 2> /dev/null
+if [ $? == 0 ]
+then
+    echo "Successfully connected to Google, no need to use Go proxy"
+else
+    echo "Google is blocked, Go proxy is enabled: GOPROXY=https://goproxy.cn,direct"
+    export GOPROXY="--build-arg GOPROXY=https://goproxy.cn,direct"
+fi
+docker buildx build --platform linux/arm64,linux/amd64 --target STANDARD $GOPROXY -t ${DOCKER_PREFIX}:${branch} -f Dockerfile --push .
