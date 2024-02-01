375df07424883c000674c95eced055cdee58b555
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2024-02-01 15:26:14 +0300

    feat: add ability to set recipient parameter name for Custom HTTP SMS provider
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

--- web/src/ProviderEditPage.js
+++ web/src/ProviderEditPage.js
@@ -1077,11 +1077,21 @@ class ProviderEditPage extends React.Component {
                     </Row>
                     <Row style={{marginTop: "20px"}} >
                       <Col style={{marginTop: "5px"}} span={(Setting.isMobile()) ? 22 : 2}>
-                        {Setting.getLabel(i18next.t("provider:Parameter"), i18next.t("provider:Parameter - Tooltip"))} :
+                        {Setting.getLabel(i18next.t("provider:Content parameter name"), i18next.t("provider:Content parameter name - Tooltip"))} :
                       </Col>
                       <Col span={22} >
                         <Input value={this.state.provider.title} onChange={e => {
                           this.updateProviderField("title", e.target.value);
+                        }} />
+                      </Col>
+                    </Row>
+                    <Row style={{marginTop: "20px"}} >
+                      <Col style={{marginTop: "5px"}} span={(Setting.isMobile()) ? 22 : 2}>
+                        {Setting.getLabel(i18next.t("provider:Recipient parameter name"), i18next.t("provider:Recipient parameter name - Tooltip"))} :
+                      </Col>
+                      <Col span={22} >
+                        <Input value={this.state.provider.metadata} onChange={e => {
+                          this.updateProviderField("metadata", e.target.value);
                         }} />
                       </Col>
                     </Row>
