c084f7b737bca5f5c339c4fe99a80ff174d16afd
DacongDA <dacongda1@sina.com>
2024-03-04 23:11:04 +0800

    fix: fix duplicate load of custom head

--- web/src/basic/CustomHead.js
+++ web/src/basic/CustomHead.js
@@ -16,37 +16,32 @@ import {useEffect} from "react";
 
 function CustomHead(props) {
   useEffect(() => {
-    const suffix = new Date().getTime().toString();
+    if (!sessionStorage.getItem("customHead")) {
+      const suffix = new Date().getTime().toString();
 
-    if (!props.headerHtml) {return;}
-    const node = document.createElement("div");
-    node.innerHTML = props.headerHtml;
+      if (!props.headerHtml) {return;}
+      const node = document.createElement("div");
+      node.innerHTML = props.headerHtml;
 
-    node.childNodes.forEach(el => {
-      if (el.nodeName === "#text") {
-        return;
-      }
-      let innerNode = el;
-      innerNode.setAttribute("app-custom-head" + suffix, "");
-
-      if (innerNode.localName === "script") {
-        const scriptNode = document.createElement("script");
-        Array.from(innerNode.attributes).forEach(attr => {
-          scriptNode.setAttribute(attr.name, attr.value);
-        });
-        scriptNode.text = innerNode.textContent;
-        innerNode = scriptNode;
-      }
-      document.head.appendChild(innerNode);
-    });
-
-    return () => {
-      for (const el of document.head.children) {
-        if (el.getAttribute("app-custom-head" + suffix) !== null) {
-          document.head.removeChild(el);
+      node.childNodes.forEach(el => {
+        if (el.nodeName === "#text") {
+          return;
         }
-      }
-    };
+        let innerNode = el;
+        innerNode.setAttribute("app-custom-head" + suffix, "");
+
+        if (innerNode.localName === "script") {
+          const scriptNode = document.createElement("script");
+          Array.from(innerNode.attributes).forEach(attr => {
+            scriptNode.setAttribute(attr.name, attr.value);
+          });
+          scriptNode.text = innerNode.textContent;
+          innerNode = scriptNode;
+        }
+        document.head.appendChild(innerNode);
+      });
+      sessionStorage.setItem("customHead", "true");
+    }
   });
 }
 
