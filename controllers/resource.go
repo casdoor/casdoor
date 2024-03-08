a92d20162a00b91f26df1f82074c9397cfa9b4b0
Yang Luo <hsluoyz@qq.com>
2024-03-08 15:03:03 +0800

    feat: show all resources for org admin

--- controllers/resource.go
+++ controllers/resource.go
@@ -52,6 +52,15 @@ func (c *ApiController) GetResources() {
 	sortField := c.Input().Get("sortField")
 	sortOrder := c.Input().Get("sortOrder")
 
+	userObj, ok := c.RequireSignedInUser()
+	if !ok {
+		return
+	}
+
+	if userObj != nil && userObj.IsAdmin {
+		user = ""
+	}
+
 	if sortField == "Direct" {
 		provider, err := c.GetProviderFromContext("Storage")
 		if err != nil {
