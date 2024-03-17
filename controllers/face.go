0844b6a9abfa9278ffccf5829ff8ea79652d9f29
Eric Luo <hsluoyz@qq.com>
2024-03-17 23:55:16 +0800

    Update face.go

--- controllers/face.go
+++ controllers/face.go
@@ -1,3 +1,20 @@
+// Copyright 2024 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Casdoor will expose its providers as services to SDK
+// We are going to implement those services as APIs here
+
 package controllers
 
 import (
@@ -18,22 +35,21 @@ import (
 func (c *ApiController) FaceIDSigninBegin() {
 	userOwner := c.Input().Get("owner")
 	userName := c.Input().Get("name")
+
 	user, err := object.GetUserByFields(userOwner, userName)
 	if err != nil {
 		c.ResponseError(err.Error())
 		return
 	}
-
 	if user == nil {
 		c.ResponseError(fmt.Sprintf(c.T("general:The user: %s doesn't exist"), util.GetId(userOwner, userName)))
 		return
 	}
+
 	if len(user.FaceIds) == 0 {
 		c.ResponseError(c.T("check:Face data does not exist, cannot log in"))
 		return
 	}
 
-	resp := &Response{Status: "ok", Msg: ""}
-	c.Data["json"] = resp
-	c.ServeJSON()
+	c.ResponseOk()
 }
