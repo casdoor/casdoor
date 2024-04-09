06e09ccbbf12257e7eaead5944adb4d291134670
DacongDA <dacongda1@sina.com>
2024-04-09 20:30:56 +0800

    feat: add support for email provider send grid

--- web/src/Setting.js
+++ web/src/Setting.js
@@ -181,6 +181,10 @@ export const OtherProviderInfo = {
       logo: `${StaticBaseUrl}/img/social_azure.png`,
       url: "https://learn.microsoft.com/zh-cn/azure/communication-services",
     },
+    "Send Grid": {
+      logo: `${StaticBaseUrl}/img/send_grid.png`,
+      url: "https://sendgrid.com/",
+    },
     "Custom HTTP Email": {
       logo: `${StaticBaseUrl}/img/social_default.png`,
       url: "https://casdoor.org/docs/provider/email/overview",
@@ -1015,6 +1019,7 @@ export function getProviderTypeOptions(category) {
         {id: "SUBMAIL", name: "SUBMAIL"},
         {id: "Mailtrap", name: "Mailtrap"},
         {id: "Azure ACS", name: "Azure ACS"},
+        {id: "Send Grid", name: "Send Grid"},
         {id: "Custom HTTP Email", name: "Custom HTTP Email"},
       ]
     );
