38e389e8c809a3183f00c3f3e0aa6e52b507adbe
Zheming Bao <56347255+EZ4Jam1n@users.noreply.github.com>
2024-08-13 16:09:16 +0800

    feat: Pagination not updating after last item deletion (#3120)

--- web/src/OrganizationListPage.js
+++ web/src/OrganizationListPage.js
@@ -115,11 +115,11 @@ class OrganizationListPage extends BaseListPage {
       .then((res) => {
         if (res.status === "ok") {
           Setting.showMessage("success", i18next.t("general:Successfully deleted"));
-          this.setState({
-            data: Setting.deleteRow(this.state.data, i),
+          this.fetch({
             pagination: {
               ...this.state.pagination,
-              total: this.state.pagination.total - 1},
+              current: this.state.pagination.current > 1 && this.state.data.length === 1 ? this.state.pagination.current - 1 : this.state.pagination.current,
+            },
           });
           window.dispatchEvent(new Event("storageOrganizationsChanged"));
         } else {
