a0d6f2125ea2f376ac7f67861015b1fe1d0a4943
ZhaoYP 2001 <75831053+ZhaoYP-2001@users.noreply.github.com>
2024-08-20 17:29:37 +0800

    feat: support reCAPTCHA v3 captcha provider (#3130)

--- web/src/Setting.js
+++ web/src/Setting.js
@@ -279,7 +279,11 @@ export const OtherProviderInfo = {
       logo: `${StaticBaseUrl}/img/captcha_default.png`,
       url: "https://pkg.go.dev/github.com/dchest/captcha",
     },
-    "reCAPTCHA": {
+    "reCAPTCHA v2": {
+      logo: `${StaticBaseUrl}/img/social_recaptcha.png`,
+      url: "https://www.google.com/recaptcha",
+    },
+    "reCAPTCHA v3": {
       logo: `${StaticBaseUrl}/img/social_recaptcha.png`,
       url: "https://www.google.com/recaptcha",
     },
@@ -1083,7 +1087,8 @@ export function getProviderTypeOptions(category) {
   } else if (category === "Captcha") {
     return ([
       {id: "Default", name: "Default"},
-      {id: "reCAPTCHA", name: "reCAPTCHA"},
+      {id: "reCAPTCHA v2", name: "reCAPTCHA v2"},
+      {id: "reCAPTCHA v3", name: "reCAPTCHA v3"},
       {id: "hCaptcha", name: "hCaptcha"},
       {id: "Aliyun Captcha", name: "Aliyun Captcha"},
       {id: "GEETEST", name: "GEETEST"},
