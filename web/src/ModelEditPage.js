e7405dbade5a9238379df591b3c04e86f9d9e09f
Coki <hashcookie404@gmail.com>
2024-09-03 17:57:09 +0800

    feat: add model syntax linting and update dependencies

--- web/src/ModelEditPage.js
+++ web/src/ModelEditPage.js
@@ -21,6 +21,9 @@ import i18next from "i18next";
 
 import {Controlled as CodeMirror} from "react-codemirror2";
 import "codemirror/lib/codemirror.css";
+import "codemirror/addon/lint/lint.css";
+import "codemirror/addon/lint/lint";
+import {newModel} from "casbin";
 
 require("codemirror/mode/properties/properties");
 
@@ -90,6 +93,44 @@ class ModelEditPage extends React.Component {
     });
   }
 
+  checkModelSyntax = (modelText) => {
+    try {
+      const model = newModel(modelText);
+      if (!model.model.get("r") || !model.model.get("p") || !model.model.get("e")) {
+        throw new Error("Model is missing one or more required sections (r, p, or e)");
+      }
+      return null;
+    } catch (e) {
+      return e.message;
+    }
+  };
+
+  createLinter = (CodeMirror) => {
+    CodeMirror.registerHelper("lint", "properties", (text) => {
+      const error = this.checkModelSyntax(text);
+      if (error) {
+        const lineMatch = error.match(/line (\d+)/);
+        if (lineMatch) {
+          const lineNumber = parseInt(lineMatch[1], 10) - 1;
+          return [{
+            from: CodeMirror.Pos(lineNumber, 0),
+            to: CodeMirror.Pos(lineNumber, text.split("\n")[lineNumber].length),
+            message: error,
+            severity: "error",
+          }];
+        } else {
+          return [{
+            from: CodeMirror.Pos(0, 0),
+            to: CodeMirror.Pos(text.split("\n").length - 1),
+            message: error,
+            severity: "error",
+          }];
+        }
+      }
+      return [];
+    });
+  };
+
   renderModel() {
     return (
       <Card size="small" title={
@@ -150,12 +191,20 @@ class ModelEditPage extends React.Component {
             <div style={{width: "100%"}} >
               <CodeMirror
                 value={this.state.model.modelText}
-                options={{mode: "properties", theme: "default"}}
+                options={{
+                  mode: "properties",
+                  theme: "default",
+                  lineNumbers: true,
+                  lint: true,
+                }}
                 onBeforeChange={(editor, data, value) => {
                   if (Setting.builtInObject(this.state.model)) {
                     return;
                   }
                   this.updateModelField("modelText", value);
+                }}
+                editorDidMount={(editor, value, cb) => {
+                  this.createLinter(editor.constructor);
                 }}
               />
             </div>
