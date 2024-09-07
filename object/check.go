ffe871f69a2e8ea6931fd21b8655f2990db5793b
dacongda <dacongda1@sina.com>
2024-09-07 09:06:55 +0800

    fix: fix return nil user

--- object/check.go
+++ object/check.go
@@ -431,7 +431,7 @@ func CheckUserPassword(organization string, username string, password string, la
 	}
 
 	err = CheckPassword(user, password, lang, options...)
-	return nil, err
+	return user, err
 }
 
 func CheckUserPermission(requestUserId, userId string, strict bool, lang string) (bool, error) {
