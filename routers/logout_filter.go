70efd60d8773bc7794eae9b3d9ea97bb84ca5b48
SuccessZ <zhaoyaopeng@seu.edu.cn>
2024-09-26 01:52:38 +0800

    fix: change the implementation of judgment statement

--- routers/logout_filter.go
+++ routers/logout_filter.go
@@ -2,10 +2,11 @@ package routers
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/beego/beego/context"
 	"github.com/beego/beego/logs"
 	"github.com/casdoor/casdoor/conf"
-	"time"
 )
 
 var (
@@ -27,15 +28,13 @@ func LogoutFilter(ctx *context.Context) {
 	if owner != "anonymous" && name != "anonymous" {
 		sessionId := ctx.Input.CruSession.SessionID()
 		currentTime := time.Now()
-		if cookieTime, exist := cookie2LastTime[sessionId]; exist {
-			if cookieTime.Add(logoutMinutes).Before(currentTime) {
-				delete(cookie2LastTime, sessionId)
-				ctx.Input.CruSession.Set("username", "")
-				ctx.Input.CruSession.Set("accessToken", "")
-				ctx.Input.CruSession.Delete("SessionData")
-				responseError(ctx, T(ctx, "auth:Long time of no operation"))
-				return
-			}
+		if cookieTime, exist := cookie2LastTime[sessionId]; exist && cookieTime.Add(logoutMinutes).Before(currentTime) {
+			delete(cookie2LastTime, sessionId)
+			ctx.Input.CruSession.Set("username", "")
+			ctx.Input.CruSession.Set("accessToken", "")
+			ctx.Input.CruSession.Delete("SessionData")
+			responseError(ctx, T(ctx, "auth:Long time of no operation"))
+			return
 		}
 		cookie2LastTime[sessionId] = currentTime
 	}
