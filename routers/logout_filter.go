98d6cd89028edc796576f85eb68ff23aed7e9c1f
SuccessZ <zhaoyaopeng@seu.edu.cn>
2024-09-26 12:41:52 +0800

    fix: syntax standards and Apache headers

--- routers/logout_filter.go
+++ routers/logout_filter.go
@@ -1,3 +1,17 @@
+// Copyright 2024 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package routers
 
 import (
@@ -16,11 +30,11 @@ var (
 )
 
 func init() {
-	logoutMinutes_int, err := conf.GetConfigInt64("logoutMinutes")
+	logoutMinutesInt, err := conf.GetConfigInt64("logoutMinutes")
 	if err != nil {
 		logs.Info(fmt.Sprintf("get logoutMinutes failed, err:%v. use default time duration: 30 minutes", err))
 	} else {
-		logoutMinutes = time.Minute * time.Duration(logoutMinutes_int)
+		logoutMinutes = time.Minute * time.Duration(logoutMinutesInt)
 	}
 }
 
