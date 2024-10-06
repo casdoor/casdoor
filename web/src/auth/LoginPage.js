c0a302faffff479c786cf84bccb129b5988d5c81
okatu-loli <CN_QianShi@hotmail.com>
2024-10-06 21:23:33 +0800

    feat: add mistake process

--- web/src/auth/LoginPage.js
+++ web/src/auth/LoginPage.js
@@ -1290,8 +1290,8 @@ class LoginPage extends React.Component {
       <React.Fragment>
         <CustomGithubCorner />
         <div className="login-content" style={{margin: this.props.preview ?? this.parseOffset(application.formOffset)}}>
-          {Setting.inIframe() || Setting.isMobile() ? null : (<div dangerouslySetInnerHTML={{__html: this.state.themeAlgorithm.includes("dark") ? application.darkModeFormCss : application.formCss}} />)}
-          {Setting.inIframe() || !Setting.isMobile() ? null : (<div dangerouslySetInnerHTML={{__html: this.state.themeAlgorithm.includes("dark") ? application.darkModeFormCssMobile : application.formCssMobile}} />)}
+          {Setting.inIframe() || Setting.isMobile() ? null : (<div dangerouslySetInnerHTML={{__html: this.props.themeAlgorithm && this.props.themeAlgorithm.includes("dark") ? application.darkModeFormCss : application.formCss}} />)}
+          {Setting.inIframe() || !Setting.isMobile() ? null : (<div dangerouslySetInnerHTML={{__html: this.props.themeAlgorithm && this.props.themeAlgorithm.includes("dark") ? application.darkModeFormCssMobile : application.formCssMobile}} />)}
           <div className="login-panel">
             <div className="side-image" style={{display: application.formOffset !== 4 ? "none" : null}}>
               <div dangerouslySetInnerHTML={{__html: application.formSideHtml}} />
