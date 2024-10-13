fc864b0de46638188a3c12b8238880f7b7e9a001
千石 <CN_QianShi@hotmail.com>
2024-10-13 22:31:54 +0800

    feat: support ".login-panel-dark" CSS for signup/login pages (#3269)
    * feat: add custom dark mode CSS for login and registration forms.
    
    * refactor: extract dark theme check to Setting.js

--- web/src/ApplicationEditPage.js
+++ web/src/ApplicationEditPage.js
@@ -46,12 +46,18 @@ require("codemirror/mode/css/css");
 const {Option} = Select;
 
 const template = `<style>
-  .login-panel{
+  .login-panel {
     padding: 40px 70px 0 70px;
     border-radius: 10px;
     background-color: #ffffff;
     box-shadow: 0 0 30px 20px rgba(0, 0, 0, 0.20);
-}
+  }
+  .login-panel-dark {
+    padding: 40px 70px 0 70px;
+    border-radius: 10px;
+    background-color: #333333;
+    box-shadow: 0 0 30px 20px rgba(255, 255, 255, 0.20);
+  }
 </style>`;
 
 const previewGrid = Setting.isMobile() ? 22 : 11;
