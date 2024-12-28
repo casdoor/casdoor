4d0f73c84e2ec282a01f41f1962cd7f6eb53d1be
Yang Luo <hsluoyz@gmail.com>
2024-12-28 10:01:56 +0800

    feat: fix Casdoor OAuth provider doesn't use domain field bug

--- object/provider.go
+++ object/provider.go
@@ -421,7 +421,7 @@ func FromProviderToIdpInfo(ctx *context.Context, provider *Provider) *idp.Provid
 			providerInfo.ClientId = provider.ClientId2
 			providerInfo.ClientSecret = provider.ClientSecret2
 		}
-	} else if provider.Type == "AzureAD" || provider.Type == "AzureADB2C" || provider.Type == "ADFS" || provider.Type == "Okta" {
+	} else if provider.Type == "ADFS" || provider.Type == "AzureAD" || provider.Type == "AzureADB2C" || provider.Type == "Casdoor" || provider.Type == "Okta" {
 		providerInfo.HostUrl = provider.Domain
 	}
 
