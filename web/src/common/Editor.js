c3a923e3243ce549a754a6defec3033231e7b59a
WindSpiritSR <simon343riley@gmail.com>
2025-02-17 00:13:03 +0800

    feat: fix style props of Editor
    Signed-off-by: WindSpiritSR <simon343riley@gmail.com>

--- web/src/common/Editor.js
+++ web/src/common/Editor.js
@@ -18,14 +18,33 @@ import {materialDark} from "@uiw/codemirror-theme-material";
 import {langs} from "@uiw/codemirror-extensions-langs";
 
 export const Editor = (props) => {
-  const fillHeight = props.fillHeight ? {
-    height: "100%",
-    style: {height: "100%"},
-  } : {};
-  const fillWidth = props.fillWidth ? {
-    width: "100%",
-    style: {width: "100%"},
-  } : {};
+  let style = {};
+  let height = props.height;
+  let width = props.width;
+  const copy2StyleProps = [
+    "width", "maxWidth", "minWidth",
+    "height", "maxHeight", "minHeight",
+  ];
+  if (props.fillHeight) {
+    height = "100%";
+    style = {...style, height: "100%"};
+  }
+  if (props.fillWidth) {
+    width = "100%";
+    style = {...style, width: "100%"};
+  }
+  /**
+   * @uiw/react-codemirror style props sucha as "height" "width"
+   * may need to be configured with "style" in some scenarios to take effect
+   */
+  copy2StyleProps.forEach(el => {
+    if (["number", "string"].includes(typeof props[el])) {
+      style = {...style, [el]: props[el]};
+    }
+  });
+  if (props.style) {
+    style = {...style, ...props.style};
+  }
   let extensions = [];
   switch (props.lang) {
   case "javascript":
@@ -50,8 +69,9 @@ export const Editor = (props) => {
     <CodeMirror
       value={props.value}
       {...props}
-      {...fillHeight}
-      {...fillWidth}
+      width={width}
+      height={height}
+      style={style}
       readOnly={props.readOnly}
       theme={props.dark ? materialDark : "light"}
       extensions={extensions}
