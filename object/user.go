99bcdfe0fd846280ff4c443cab91ee41823f0ecc
DacongDA <dacongda1@sina.com>
2025-04-08 12:32:18 +0800

    fix: unable to query webauthnCredentials when dbtype equal to mssql or postgresql

--- object/user.go
+++ object/user.go
@@ -461,7 +461,19 @@ func GetUserByEmail(owner string, email string) (*User, error) {
 
 func GetUserByWebauthID(webauthId string) (*User, error) {
 	user := User{}
-	existed, err := ormer.Engine.Where("webauthnCredentials LIKE ?", "%"+webauthId+"%").Get(&user)
+	existed := false
+	var err error
+
+	//res, err := ormer.Engine.Query("select * from [user] where CAST(webauthnCredentials AS VARCHAR(MAX)) like ?", "%"+webauthId+"%")
+	//print(res)
+
+	if ormer.driverName == "postgres" {
+		existed, err = ormer.Engine.Where(builder.Like{"\"webauthnCredentials\"", webauthId}).Get(&user)
+	} else if ormer.driverName == "mssql" {
+		existed, err = ormer.Engine.Where("CAST(webauthnCredentials AS VARCHAR(MAX)) like ?", "%"+webauthId+"%").Get(&user)
+	} else {
+		existed, err = ormer.Engine.Where("webauthnCredentials like ?", "%"+webauthId+"%").Get(&user)
+	}
 	if err != nil {
 		return nil, err
 	}
