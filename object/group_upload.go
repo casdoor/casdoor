0a072b2dccb8e6bbbe67ed428d77c492b87c2962
Attack825 <2707138687@qq.com>
2025-06-17 21:34:04 +0800

    chore: add Apache license header to files

--- object/group_upload.go
+++ object/group_upload.go
@@ -1,3 +1,17 @@
+// Copyright 2021 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package object
 
 import (
@@ -24,7 +38,6 @@ func StringArrayToGroup(stringArray [][]string) ([]*Group, error) {
 			continue
 		}
 
-		// 检查是否为空行
 		isEmptyRow := true
 		for _, val := range field {
 			if strings.TrimSpace(val) != "" {
@@ -33,7 +46,7 @@ func StringArrayToGroup(stringArray [][]string) ([]*Group, error) {
 			}
 		}
 		if isEmptyRow {
-			continue // 跳过空行
+			continue
 		}
 
 		tempMap := map[string]string{}
@@ -118,7 +131,7 @@ func UploadGroups(owner string, path string) (bool, error) {
 		return false, err
 	}
 
-	transGroups, err := StringArrayToGroup(table)
+	transGroups, err := StringArrayToStruct[Group](table)
 	if err != nil {
 		return false, err
 	}
