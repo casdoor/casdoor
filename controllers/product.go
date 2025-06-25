b36fb50239dc91929dc458f2953cd95c460738b5
XiangYe <xiangyegit@gmail.com>
2025-06-25 10:49:20 +0800

    feat: fix check bug to allow logged-in users to buy product (#3897)

--- controllers/product.go
+++ controllers/product.go
@@ -182,7 +182,7 @@ func (c *ApiController) BuyProduct() {
 	paidUserName := c.Input().Get("userName")
 	owner, _ := util.GetOwnerAndNameFromId(id)
 	userId := util.GetId(owner, paidUserName)
-	if paidUserName != "" && !c.IsAdmin() {
+	if paidUserName != "" && paidUserName != c.GetSessionUsername() && !c.IsAdmin() {
 		c.ResponseError(c.T("general:Only admin user can specify user"))
 		return
 	}
