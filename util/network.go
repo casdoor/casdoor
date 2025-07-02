b4d7da9121ea9929b627dd26c29d69de98077998
Ye Zhenrong <3471626272@qq.com>
2025-07-02 15:33:38 +0800

    feat: use Go standard library in network.go

--- util/network.go
+++ util/network.go
@@ -20,97 +20,20 @@ import (
 
 // IsPrivateIP checks if the given IP is a private IP address (internal network IP)
 func IsPrivateIP(ip string) bool {
-	// Parse the IP address
 	parsedIP := net.ParseIP(ip)
 	if parsedIP == nil {
 		return false
 	}
-
-	// Check if it's an IPv4 address
-	if parsedIP.To4() != nil {
-		// IPv4 private address ranges:
-		// 10.0.0.0/8     (10.0.0.0 - 10.255.255.255)
-		// 172.16.0.0/12  (172.16.0.0 - 172.31.255.255)
-		// 192.168.0.0/16 (192.168.0.0 - 192.168.255.255)
-
-		// 10.0.0.0/8
-		if parsedIP[12] == 10 {
-			return true
-		}
-
-		// 172.16.0.0/12
-		if parsedIP[12] == 172 && parsedIP[13] >= 16 && parsedIP[13] <= 31 {
-			return true
-		}
-
-		// 192.168.0.0/16
-		if parsedIP[12] == 192 && parsedIP[13] == 168 {
-			return true
-		}
-
-		// 127.0.0.0/8 (loopback address)
-		if parsedIP[12] == 127 {
-			return true
-		}
-
-		// 169.254.0.0/16 (link-local address)
-		if parsedIP[12] == 169 && parsedIP[13] == 254 {
-			return true
-		}
-	} else {
-		// IPv6 private addresses
-		// fc00::/7 (unique local address)
-		if parsedIP[0] >= 0xfc && parsedIP[0] <= 0xfd {
-			return true
-		}
-
-		// fe80::/10 (link-local address)
-		if parsedIP[0] == 0xfe && (parsedIP[1]&0xc0) == 0x80 {
-			return true
-		}
-
-		// ::1/128 (loopback address)
-		if parsedIP.IsLoopback() {
-			return true
-		}
-	}
-
-	return false
+	return parsedIP.IsPrivate()
 }
 
 // IsInternetIp checks if the given IP is a public IP address (external network IP)
 func IsInternetIp(ip string) bool {
-	// Parse the IP address
 	parsedIP := net.ParseIP(ip)
 	if parsedIP == nil {
 		return false
 	}
 
-	if IsPrivateIP(ip) {
-		return false
-	}
-
-	if parsedIP.IsMulticast() {
-		return false
-	}
-
-	if parsedIP.IsUnspecified() {
-		return false
-	}
-
-	// For IPv4, need to exclude some special address ranges
-	if parsedIP.To4() != nil {
-		// 0.0.0.0/8 (this network)
-		if parsedIP[12] == 0 {
-			return false
-		}
-
-		// 224.0.0.0/4 (multicast addresses, already checked by IsMulticast)
-		// 240.0.0.0/4 (reserved addresses)
-		if parsedIP[12] >= 240 {
-			return false
-		}
-	}
-
-	return true
+	return !parsedIP.IsPrivate() && !parsedIP.IsLoopback() && 
+		   !parsedIP.IsMulticast() && !parsedIP.IsUnspecified()
 }
