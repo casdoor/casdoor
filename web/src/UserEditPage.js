9296dede95aba24ec47f9bc59ecd0076581f7690
Yang Luo <hsluoyz@gmail.com>
2025-11-01 09:20:28 +0800

    Update UserEditPage.js

--- web/src/UserEditPage.js
+++ web/src/UserEditPage.js
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 import React from "react";
 import {Button, Card, Col, Form, Input, InputNumber, List, Result, Row, Select, Space, Spin, Switch, Tag, Tooltip} from "antd";
 import {withRouter} from "react-router-dom";
