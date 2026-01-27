f61af1b497857338197180780ea810d699672a6f
mayunxiang <mayunxiang664@gmail.com>
2026-01-27 12:28:13 +0800

    feat: Optimize the display of the products column on the order and payment, adjust the color of the “Add to Cart” button.

--- object/payment.go
+++ object/payment.go
@@ -50,7 +50,8 @@ type Payment struct {
 	InvoiceRemark string `xorm:"varchar(100)" json:"invoiceRemark"`
 	InvoiceUrl    string `xorm:"varchar(255)" json:"invoiceUrl"`
 	// Order Info
-	Order      string          `xorm:"varchar(100)" json:"order"`      // Internal order name
+	Order      string          `xorm:"varchar(100)" json:"order"` // Internal order name
+	OrderObj   *Order          `xorm:"-" json:"orderObj,omitempty"`
 	OutOrderId string          `xorm:"varchar(100)" json:"outOrderId"` // External payment provider's order ID
 	PayUrl     string          `xorm:"varchar(2000)" json:"payUrl"`
 	SuccessUrl string          `xorm:"varchar(2000)" json:"successUrl"` // `successUrl` is redirected from `payUrl` after pay success
@@ -70,12 +71,22 @@ func GetPayments(owner string) ([]*Payment, error) {
 		return nil, err
 	}
 
+	err = ExtendPaymentWithOrder(payments)
+	if err != nil {
+		return nil, err
+	}
+
 	return payments, nil
 }
 
 func GetUserPayments(owner, user string) ([]*Payment, error) {
 	payments := []*Payment{}
 	err := ormer.Engine.Desc("created_time").Find(&payments, &Payment{Owner: owner, User: user})
+	if err != nil {
+		return nil, err
+	}
+
+	err = ExtendPaymentWithOrder(payments)
 	if err != nil {
 		return nil, err
 	}
@@ -91,7 +102,24 @@ func GetPaginationPayments(owner string, offset, limit int, field, value, sortFi
 		return nil, err
 	}
 
+	err = ExtendPaymentWithOrder(payments)
+	if err != nil {
+		return nil, err
+	}
+
 	return payments, nil
+}
+
+func ExtendPaymentWithOrder(payments []*Payment) error {
+	for _, payment := range payments {
+		if payment.Order != "" {
+			order, err := getOrder(payment.Owner, payment.Order)
+			if err == nil && order != nil {
+				payment.OrderObj = order
+			}
+		}
+	}
+	return nil
 }
 
 func getPayment(owner string, name string) (*Payment, error) {
