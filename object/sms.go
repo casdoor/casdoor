cce7a3ba1980e2e79a1cb7221ef51975d5776ba2
cont1nu1ty <808535fm@gmail.com>
2026-03-06 21:28:01 +0800

    fix: Fix Twilio SMS sending %!(EXTRA string=...) and refactor ifelses to switch

--- object/sms.go
+++ object/sms.go
@@ -26,19 +26,23 @@ func getSmsClient(provider *Provider) (sender.SmsClient, error) {
 	var client sender.SmsClient
 	var err error
 
-	if provider.Type == sender.HuaweiCloud || provider.Type == sender.AzureACS {
+	switch provider.Type {
+	case sender.HuaweiCloud, sender.AzureACS:
 		client, err = sender.NewSmsClient(provider.Type, provider.ClientId, provider.ClientSecret, provider.SignName, provider.TemplateCode, provider.ProviderUrl, provider.AppId)
-	} else if provider.Type == "Custom HTTP SMS" {
+	case "Custom HTTP SMS":
 		client, err = newHttpSmsClient(provider.Endpoint, provider.Method, provider.Title, provider.TemplateCode, provider.HttpHeaders, provider.UserMapping, provider.IssuerUrl, provider.EnableProxy)
-	} else if provider.Type == "Alibaba Cloud PNVS SMS" {
+	case "Alibaba Cloud PNVS SMS":
 		client, err = newPnvsSmsClient(provider.ClientId, provider.ClientSecret, provider.SignName, provider.TemplateCode, provider.RegionId)
-	} else {
+	case sender.Twilio:
+		client, err = sender.NewSmsClient(provider.Type, provider.ClientId, provider.ClientSecret, provider.SignName, "%s", provider.AppId)
+	default:
+		// 默认初始化方式：适用于阿里云、腾讯云等大多数厂商
 		client, err = sender.NewSmsClient(provider.Type, provider.ClientId, provider.ClientSecret, provider.SignName, provider.TemplateCode, provider.AppId)
 	}
+
 	if err != nil {
 		return nil, err
 	}
-
 	return client, nil
 }
 
@@ -48,30 +52,38 @@ func SendSms(provider *Provider, content string, phoneNumbers ...string) error {
 		return err
 	}
 
-	if provider.Type == sender.Twilio {
-		if provider.AppId != "" {
-			phoneNumbers = append([]string{provider.AppId}, phoneNumbers...)
-		}
-	} else if provider.Type == sender.Aliyun || provider.Type == "Alibaba Cloud PNVS SMS" {
+	if provider.Type == sender.Aliyun || provider.Type == "Alibaba Cloud PNVS SMS" {
 		for i, number := range phoneNumbers {
 			phoneNumbers[i] = strings.TrimPrefix(number, "+86")
 		}
 	}
 
-	params := map[string]string{}
-	if provider.Type == sender.TencentCloud {
-		params["0"] = content
-	} else {
-		params["code"] = content
-		if provider.Type == "Alibaba Cloud PNVS SMS" {
-			timeoutInMinutes, err := conf.GetConfigInt64("verificationCodeTimeout")
-			if err != nil || timeoutInMinutes <= 0 {
-				timeoutInMinutes = 10
-			}
-			params["min"] = strconv.FormatInt(timeoutInMinutes, 10)
+	if provider.Type == sender.Twilio {
+		if provider.AppId != "" {
+			phoneNumbers = append([]string{provider.AppId}, phoneNumbers...)
+		}
+		if strings.Contains(provider.TemplateCode, "%s") {
+			content = strings.Replace(provider.TemplateCode, "%s", content, 1)
 		}
 	}
 
-	err = client.SendMessage(params, phoneNumbers...)
-	return err
+	params := map[string]string{}
+
+	switch provider.Type {
+	case sender.TencentCloud:
+		params["0"] = content
+
+	case "Alibaba Cloud PNVS SMS":
+		params["code"] = content
+		timeoutInMinutes, err := conf.GetConfigInt64("verificationCodeTimeout")
+		if err != nil || timeoutInMinutes <= 0 {
+			timeoutInMinutes = 10
+		}
+		params["min"] = strconv.FormatInt(timeoutInMinutes, 10)
+
+	default:
+		params["code"] = content
+	}
+
+	return client.SendMessage(params, phoneNumbers...)
 }
