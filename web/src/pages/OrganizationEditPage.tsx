157db2936f9f20188753536e8c0e35fe81f9f5a7
Vika Lvovich <vikalvovich@users.noreply.github.com>
2026-09-18 14:47:36 +0300

    fix: use unique i18n keys for organization exclusive signin
    TestDeduplicateFrontendI18n forbids the same key name under application and organization.

--- web/src/pages/OrganizationEditPage.tsx
+++ web/src/pages/OrganizationEditPage.tsx
@@ -316,7 +316,7 @@ export default function OrganizationEditPage() {
           <FormRow labelKey="application:Disable signin">
             <Switch checked={!!organization.disableSignin} onCheckedChange={(v) => update("disableSignin", v)} />
           </FormRow>
-          <FormRow labelKey="organization:Enable exclusive signin">
+          <FormRow labelKey="organization:Enable exclusive signin across applications">
             <Switch checked={!!organization.enableExclusiveSignin} onCheckedChange={(v) => update("enableExclusiveSignin", v)} />
           </FormRow>
           <FormRow labelKey="organization:Disable console">
